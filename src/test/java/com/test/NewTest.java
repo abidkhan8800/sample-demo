2c434b5e50aba40dce1b4e95e503619d5ede138e
abidkhan8800 <khanabid844708@gmail.com>
2022-01-06 12:56:27 +0530

    first-commit

---
+++ src/test/java/com/test/NewTest.java
@@ -0,0 +1,19 @@
+package com.test;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  ChromeOptions options = new ChromeOptions();
+	  options.addArguments("--headless");
+	  ChromeDriver driver = new ChromeDriver(options);
+	  driver.get("https://www.google.com");
+	  System.out.println(driver.getCurrentUrl());
+	  Assert.assertTrue(true);
+	  driver.close();
+  }
+}
